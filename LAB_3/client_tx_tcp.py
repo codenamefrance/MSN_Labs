71fcc3f7721ab7b5c0ffa29106141f6896ad00cf
codenamefrance <francescopiocorrado@outlook.it>
2026-05-19 17:16:40 +0200

    fix tcp

--- LAB_3/client_tx_tcp.py
+++ LAB_3/client_tx_tcp.py
@@ -2,24 +2,24 @@ import socket
 import time
 
 # !!! SOSTITUISCI CON L'IP REALE DEL TUO PC LINUX !!!
-UDP_IP = "IP_DEL_TUO_PC_LINUX" [cite: 71]
-UDP_PORT = 47999 [cite: 73]
+UDP_IP = "172.21.156.91" 
+UDP_PORT = 47999 
 MESSAGE_STRING = "STRINGA" # Scegli una parola a tua scelta (senza spazi) [cite: 57, 74]
 
-print("UDP server IP: %s" % UDP_IP) [cite: 75]
-print("UDP destination port: %s" % UDP_PORT) [cite: 76]
+print("UDP server IP: %s" % UDP_IP) 
+print("UDP destination port: %s" % UDP_PORT)
 
 count = 0 # Contatore dei pacchetti inviati [cite: 77]
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Creazione socket IPv4 + UDP [cite: 79]
 
-while count < 30: [cite: 80]
-    count = count + 1 [cite: 81]
+while count < 30:
+    count = count + 1 
     
     # Composizione del messaggio senza spazi extra: stringa,virgola,sequenza$ [cite: 57, 82]
-    message = MESSAGE_STRING + "," + str(count) + "$" [cite: 82]
-    print("sending message: %s" % message) [cite: 82]
+    message = MESSAGE_STRING + "," + str(count) + "$" 
+    print("sending message: %s" % message) 
     
     # Conversione in byte (encode) e invio al server [cite: 62, 83]
-    sock.sendto(message.encode(), (UDP_IP, UDP_PORT)) [cite: 83]
+    sock.sendto(message.encode(), (UDP_IP, UDP_PORT)) 
     
     time.sleep(0.2) # Attesa obbligatoria di 200 ms [cite: 57, 84]
--- LAB_3/server_rx_tcp.py
+++ LAB_3/server_rx_tcp.py
@@ -1,8 +1,8 @@
 import socket
 
 # "0.0.0.0" indica di ascoltare su tutte le interfacce di rete del PC Linux
-UDP_IP = "0.0.0.0" [cite: 86, 88]
-UDP_PORT = 47999 [cite: 88]
+UDP_IP = "0.0.0.0" 
+UDP_PORT = 47999 
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Creazione socket IPv4 + UDP [cite: 89]
 sock.bind((UDP_IP, UDP_PORT)) # Associazione del socket all'IP e alla porta [cite: 89]
@@ -10,13 +10,13 @@ sock.bind((UDP_IP, UDP_PORT)) # Associazione del socket all'IP e alla porta [cit
 count = 0 # Contatore dei pacchetti ricevuti finora [cite: 60, 90]
 print("Server UDP pronto e in ascolto sulla porta %s..." % UDP_PORT)
 
-while True: [cite: 90]
+while True: 
     # Ricezione del pacchetto (buffer impostato a 512 byte come da specifiche) [cite: 61, 91]
-    data, addr = sock.recvfrom(512) [cite: 91]
-    count = count + 1 [cite: 91]
+    data, addr = sock.recvfrom(512)
+    count = count + 1 
     
     # Decodifica dei byte in stringa e separazione dei campi tramite la virgola [cite: 63, 92]
-    fields = data.decode().split(",") [cite: 92]
+    fields = data.decode().split(",") 
     
     # Stampa formattata: stringa, numero di sequenza del pacchetto, conteggio totale [cite: 60, 93]
-    print("received message: %s, %s, %d" % (fields[0], fields[1], count)) [cite: 93]
+    print("received message: %s, %s, %d" % (fields[0], fields[1], count)) 
